30ea7cf148c25734bdd4df74c28744d3e1241e6b
Juan <fersot100@yahoo.com>
2017-01-21 13:06:13 -0800

    Comments on conversions.js

--- conversions.js
+++ conversions.js
@@ -1,22 +1,22 @@
 var rand, toType, fromType;
 var correct = document.getElementById('correct');
-var next = document.getElementById('next');
+var next = document.getElementById('next'); // Gets an html element and assigns it to next
 
 function getAnswer() {
-	var answer = document.getElementById('answer');
+	var answer = document.getElementById('answer'); // Gets an html element and assigns it to answer
 	return answer;
 }
-function generateQuestion() {
+function generateQuestion() { 
 	answer = getAnswer();
 	answer.value = '';
 	correct.innerHTML = '';
-	correct.style.display = 'none';
+	correct.style.display = 'none'; // Initialize the html objects when question generates.
 	
 	var max = 225, min = 1, base , fromInt;
-	rand = Math.floor(Math.random() * (max - min) + min);
+	rand = Math.floor(Math.random() * (max - min) + min); //Math is part of javascript and gives you sweet functions
 	do{
 		fromType = Math.floor(Math.random() * (5 - 1) + 1);
-		toType = Math.floor(Math.random() * (5 - 1) + 1);
+		toType = Math.floor(Math.random() * (5 - 1) + 1); // These generate random numbers to pick what the base is going to be and they get passed into the switch statement 
 	}while(fromType === toType);
 	switch(toType) {
 	    case 1:
@@ -61,7 +61,7 @@ function generateQuestion() {
 	var question = "Convert the " + fromType + " integer " + fromInt + " to " + base;
 	document.getElementById('Question').innerHTML = question;
 }
-function checkAnswer () {
+function checkAnswer () { // Here I
 	userAnswer = (getAnswer()).value;
 	answer = rand.toString(toType);
 
@@ -70,7 +70,7 @@ function checkAnswer () {
 		' answer: ' + answer + ' Type: ' + (typeof answer)
 		);
 	if(answer.toUpperCase() == userAnswer.toUpperCase()){
-		correct.style.color = 'green';
+		correct.style.color = 'green'; // This is how 
 		correct.innerHTML = 'Correct! ';
 		correct.style.display = 'inline';
 	
@@ -80,11 +80,11 @@ function checkAnswer () {
 		correct.style.display = 'inline';
 	}
 }
-window.onload = function () {
+window.onload = function () { //Whenever website loads, it generates a question
 	generateQuestion();
 }
 
-$("#main-btn").click(function() {
+$("#main-btn").click(function() { // I change the text and then use that text to determine what the button does. If it says Submit I submit and vice versa
 	if ($(this).html() == 'Submit'){
 		checkAnswer();
 		$(this).html('Next');
@@ -94,7 +94,7 @@ $("#main-btn").click(function() {
 	}
 });
 
-$(document).ready(function(){
+$(document).ready(function(){  // This is so it listens for me to press enter. When I press enter it is equivalent to clicking the main button
     $('#answer').keypress(function(e){
       if(e.keyCode==13)
       $('#main-btn').click();
