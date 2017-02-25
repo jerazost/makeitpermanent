4fd57296ca2d2f1b772eee4ed1a8d4b2e2fe3fa0
Jenna Sturgeon <jenna.sturgeon@yahoo.com>
2017-02-25 12:52:23 -0800

    added morse.js

--- morse.js
+++ morse.js
@@ -1,4 +1,51 @@
-var toggleKey = true;
-function showAnswerKey(){
+var result = {}, lValues, lLength;
+var wordArray;
+var correct; //need to include addElemById;
+var next; //need to include addElemById;
+
+function getAnswer() {
+
+	var answer = document.getElementById('answer'); //melanie needs to create this html element
+
+	return answer
+}
+
+
+
+
+function generateQuestion() {
+	answer = getAnswer();
+	//answer.value = '';
+
+	var word = wordArray[Math.floor(Math.random() * 1000)];
+
+
+	document.getElementById('word').innerHTML = word;
+	
+
 
 }
+
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+    	word = JSON.parse(xhr.responseText);
+    	
+    	
+
+    	
+
+    }
+}
+
+
+
+xhr.open('GET', 'https://mjohnson196.github.io/words.json', true);
+xhr.send(null);
+
+function checkAnswer() {
+
+}
+
+
