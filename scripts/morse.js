333e85f9e31ddb7be2ae260d5a1d493def3002ad
Juan <fersot100@yahoo.com>
2017-02-28 12:43:40 -0800

    Working on dynamic button

--- scripts/morse.js
+++ scripts/morse.js
@@ -4,6 +4,7 @@ var translations = [{key:"a", value:".-"},{key:"b", value:"-..."}, {key: "c", va
 {key: "s", value: "..."}, {key: "t", value: "-"}, {key: "u", value: "..-"}, {key: "v", value: "...-"}, {key: "w", value: ".--"}, {key: "x", value: "-..-"},
 {key: "y", value: "-.--"}, {key: "z", value: "--.."}];
 var generatedWord;
+var submit = document.getElementById('submit');
 
 window.onload = function () {
 	
@@ -15,6 +16,7 @@ function generateQuestion() {
 	generatedWord = wordArray[Math.floor(Math.random() * 1000)];
 	var morse = document.getElementById('morse');
 	morse.innerHTML = convertToMorse(generatedWord);
+	console.log(generatedWord);
 
 }
 
@@ -36,8 +38,12 @@ function convertToMorse(regString){
 }
 function checkAnswer() {
 	var answer = document.getElementById('answer_input');
-	if(answer.value == generatedWord){
-
+	var result = document.getElementById('result');
+	
+	if(answer.value.toLowerCase() == generatedWord){
+		result.innerHTML = 'Correct!';
+	} else {
+		result.innerHTML = 'Wrong! The word is ' + generatedWord;
 	}
 }
 function giveUp() {
@@ -45,3 +51,19 @@ function giveUp() {
 }
 function getAnswer() {
 }
+$("#main-btn").click(function() { // I change the text and then use that text to determine what the button does. If it says Submit I submit and vice versa
+	console.log('click');
+	if ($(this).html() == 'submit'){
+		checkAnswer();
+		$(this).html('next');
+	}else if ($(this).html() == 'next'){
+		generateQuestion();
+		$(this).html('submit');
+	}
+});
+$(document).ready(function(){  // This is so it listens for me to press enter. When I press enter it is equivalent to clicking the main button
+    $('#answer').keypress(function(e){
+      if(e.keyCode==13)
+      $('#main-btn').click();
+    });
+});
