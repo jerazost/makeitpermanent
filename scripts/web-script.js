9400272bce385ad9487739ec6bfda04f74e8ec0b
Dustin Podell <magicdustin14@gmail.com>
2017-03-01 00:47:07 -0800

    fixed up some issues with AJAX, test push to main

--- scripts/web-script.js
+++ scripts/web-script.js
@@ -1,25 +1,73 @@
 
-function createUser() {
+function pushUserInfo(urlEnd) {
     var body = {};
-    name = document.getElementById('user');
-    password = document.getElementById('password');
+    var name = document.getElementById('user');
+    var password = document.getElementById('password');
 
-    
-    
+////////////////////////////
+
+
+
+
+
+/////////////////////////////
+
     body.name = name.value;
+    body.password = password.value;
+    console.log('Name: ', name.value);
+    console.log('Password: ', password.value);
+    console.log('Body Name: ' , body.name);
+    console.log('Body password: ', body.password);
+
+
+    alert(JSON.stringify(body));
+    $.ajax({
+        url: `https://lit-lowlands-87980.herokuapp.com/api/${urlEnd}`,
+        type: 'POST',
+        crossDomain: true,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(body),
+        success: function(data){alert(data)},
+        failure: function(jqXHR, textStatus, errorThrown) {
+      alert(jqXHR.responseText || textStatus);
+    }
+
+
+
+    // }).done(function(data) {
+    //     alert(data);
+    // }).error(function (jqXHR, textStatus, errorThrown) {
+
+    });
+}
+
+function pullUserInfo(urlEnd) {
+    var body = {};
+    var name = document.getElementById('user');
+    var password = document.getElementById('password');
+
+
+
+    body.name = name.value;
+    body.password = password.value;
     console.log('Name', name.value);
     console.log('Password', password.value);
     console.log('Body Name', body.name);
+    console.log('Body password', body.password);
 
-    body.password = password.value;
+
     alert(JSON.stringify(body));
     $.ajax({
-        type: "POST",
-        url: "https://lit-lowlands-87980.herokuapp.com/users",
-        data: {'': JSON.stringify(body)}
-    }).done(function(data) {
-        alert(data);
-    }).error(function (jqXHR, textStatus, errorThrown) {
+        url: `https://lit-lowlands-87980.herokuapp.com/api/${urlEnd}`,
+        type: 'GET',
+        contentType: 'application/json',
+        data: JSON.stringify(body),
+        success: (res) => {
+          console.log('HERES IS WHAT YOU GET BACK');
+          console.log(JSON.stringify(res)); //TODO: add stuff here
+        }
+      }).error(function (jqXHR, textStatus, errorThrown) {
         alert(jqXHR.responseText || textStatus);
     });
 }
